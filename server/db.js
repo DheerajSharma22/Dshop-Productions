79b752e91d01129c53fc45f31622133b97ab20af
Dheeraj Sharma <dsprofessional12@gmail.com>
2024-03-16 09:35:52 +0530

    updates

--- server/db.js
+++ server/db.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 
+
 const connectToMongo = () => {
+    console.log(process.env.DB_URI);
     mongoose
     .connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      family: 4,
     })
     .then(() => console.log("Conection Success"))
     .catch((err) => console.log(err));
